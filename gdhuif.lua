8f467e71351f7c1ca6b4c15683afb983c4609fc5
Diapolo69 <nigsnoobs@gmail.com>
2024-10-15 18:56:54 +0200

    Update gdhuif.lua

--- gdhuif.lua
+++ gdhuif.lua
@@ -1,1 +1,2 @@
-game:GetService('TeleportService'):TeleportToPlaceInstance(15502339080, '9157b097-6271-49f9-a693-0a4a2c52a1b0', game.Players.LocalPlayer)
+_G.JobId = "9157b097-6271-49f9-a693-0a4a2c52a1b0"
+game:GetService('TeleportService'):TeleportToPlaceInstance(15502339080, _G.JobId, game.Players.LocalPlayer)
